f694f040718a58b618f471a76538316863fab378
swati.suthar@bacancy.com <swati.suthar@bacancytechnology.com>
2022-10-20 11:14:38 +0530

    mail address change for others

--- app/Http/Controllers/Api/SalesController.php
+++ app/Http/Controllers/Api/SalesController.php
@@ -746,7 +746,7 @@ class SalesController extends Controller
 
     public function sendMailWithPDF(Request $request)
     {
-        $data["email"] = "swati.suthar@bacancy.com";
+        $data["email"] = "ishita.shah@bacancy.com";
         //$data["email"] = $request->input('customeremail');
         $data["title"] = "Welcome to Gold Bank Accounting Portal";
         $data["body"] = "This is the email body.";
